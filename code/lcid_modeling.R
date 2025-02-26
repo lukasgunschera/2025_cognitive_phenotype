37229cd717d78c5c72d486322a5eeebd3e503b40
Lukas Gunschera <l.gunschera@outlook.com>
2025-02-26 12:22:55 +0000

    add modeling script

---
+++ code/lcid_modeling.R
@@ -0,0 +1,338 @@
+## ============================================================================================================================
+## Script:       MODEL DELAY DISCOUNTING HYPERBOLIC FUNCTION
+## ============================================================================================================================ ##
+## Authors:      Lukas Gunschera
+## Contact:      l.gunschera@outlook.com
+##
+## Date created: 2024-07-12
+## ============================================================================================================================ ##
+##
+## Prior to executing this script ensure to set parameters FIT_MODEL, FIT_ALLDATA, and FIT_CLUSTER to appropriate values.
+## FIT_MODEL set to true executes script, set to false skips model fitting.
+## FIT_ALLDATA set to true fits model to all participants, set to false fits model to a subset of participants.
+## FIT_CLUSTER set to true fits model on high performance computing cluster, set to false fits model on local machine.
+##
+## Importantly, FIT_CLUSTER == TRUE is hard-coded and may require changes to path for the stan executable.
+##
+## ============================================================================================================================ ##
+
+## SETUP ====================================================================================================================
+
+library(renv)
+renv::restore()
+
+set.seed(777) # set seed for random processes
+
+# load required packages
+library(loo)
+library(here)
+library(purrr)
+library(readr)
+library(haven)
+library(tidyr)
+library(dplyr)
+library(hexbin)
+library(stringr)
+library(ggplot2)
+library(ggpmisc)
+library(viridis)
+library(parallel)
+library(magrittr)
+library(cmdstanr)
+library(hBayesDM)
+library(bayesplot)
+library(patchwork)
+library(ggcorrplot)
+library(viridisLite)
+
+# set global parameters determining the model fitting process
+FIT_MODEL   <- TRUE     # TRUE will fit model to data, FALSE will skip model fitting
+FIT_ALLDATA <- TRUE     # TRUE will fit model to all participants, FALSE will fit model to a subset of participants
+FIT_CLUSTER <- FALSE    # TRUE will fit model on high performance computing cluster (hard-coded and may require adjustments)
+
+ITER_WARMUP <- 2000     # Set warm up iterations for later models
+ITER_SAMPLING <- 10000  # Set sampling iterations for later models
+
+NUM_CORES <- parallel::detectCores() - 1  # Set cores to use for computation
+
+# set directory
+here::i_am("renv.lock") # set directory
+
+# load custom functions
+source(here::here("code", "functions", "fun_plots.R"))
+source(here::here("code", "functions", "fun_helper.R"))
+source(here::here("code", "functions", "fun_get_params.R"))
+source(here::here("code", "functions", "fun_convergence_check.R"))
+source(here::here("code", "functions", "fun_model_preprocessing.R"))
+source(here::here("code", "functions", "fun_posterior_predictions.R"))
+
+#### Load data --------------------------------------------------------------------------------------------------------------
+
+dd_data <- readRDS(here::here("data", "processed", "dd_task.Rds")) # task data loading
+
+# @k      = discounting rate
+# @beta   = inverse temperature
+# FIT_CLUSTER == FALSE == subset three subjects from dataframe to illustrate modeling procedure
+# FIT_CLUSTER == TRUE  == subset entire dataset and continue with full modeling procedure
+
+task_data_02 <- subset_x_participants(dd_data[[1]], FIT_ALLDATA, participants = 5)
+task_data_03 <- subset_x_participants(dd_data[[2]], FIT_ALLDATA, participants = 5)
+task_data_04 <- subset_x_participants(dd_data[[3]], FIT_ALLDATA, participants = 5)
+task_data_05 <- subset_x_participants(dd_data[[4]], FIT_ALLDATA, participants = 5)
+task_data_06 <- subset_x_participants(dd_data[[5]], FIT_ALLDATA, participants = 5)
+
+# SET MODELING PARAMETERS ---------------------------------------------------------------------------------------------------
+# setting parameters depending on cluster true/false
+
+if (FIT_MODEL && FIT_CLUSTER) {
+
+  # set cmd_stan installation path to location on cluster (change as required)
+  set_cmdstan_path("/group/orben/software/linux/cmdstan/cmdstan-2.33.1")
+
+  # Load Model (Linux)
+  dd_hyperbolic_stan <- cmdstanr::cmdstan_model(
+    here::here("code", "stan", "linux", "dd_hyperbolic.stan"))
+
+  print("MODEL WILL RUN ON CLUSTER")
+
+} else if (FIT_MODEL && !FIT_CLUSTER) {
+
+  # cmd_stan installation path should be set automatically, check installation path if required
+  # set_cmdstan_path("insert installation path")
+
+  print("MODEL WILL RUN ON LOCAL MACHINE, CODE EXECUTION MAY TAKE LONGER")
+
+  # Load Model (Mac & Windows)
+  dd_hyperbolic_stan <- cmdstanr::cmdstan_model(
+    here::here("code", "stan", "dd_hyperbolic.stan"))
+
+} else {
+
+  # output message
+  print("DID NOT RUN MODELS, YOU CAN LOAD RESULTS FROM FILE")
+}
+
+
+# PROCESS MODEL DATA --------------------------------------------------------------------------------------------------------
+process_task_data <- function(task_data) {
+  model_preprocessing(
+    raw_data = task_data,
+    retest = FALSE,
+    subjs = unique(task_data$subjID),
+    n_subj = length(unique(task_data$subjID)),
+    t_subjs = aggregate(trial ~ subjID, FUN = max, data = task_data)[, 2],
+    t_max = max(aggregate(trial ~ subjID, FUN = max, data = task_data)[, 2])
+  )
+}
+
+dd_model_dat_02 <- process_task_data(task_data_02)
+dd_model_dat_03 <- process_task_data(task_data_03)
+dd_model_dat_04 <- process_task_data(task_data_04)
+dd_model_dat_05 <- process_task_data(task_data_05)
+dd_model_dat_06 <- process_task_data(task_data_06)
+
+dd_subj_02 <- unique(task_data_02$subjID)
+dd_subj_03 <- unique(task_data_03$subjID)
+dd_subj_04 <- unique(task_data_04$subjID)
+dd_subj_05 <- unique(task_data_05$subjID)
+dd_subj_06 <- unique(task_data_06$subjID)
+
+saveRDS(list(dd_subj_02, dd_subj_03, dd_subj_04, dd_subj_05, dd_subj_06),
+        here::here("output", "lcid", "dd_delaydiscount", "modelfit", "dd_hyperbolic", "dd_subj.RDS"))
+
+if (FIT_MODEL) {
+
+  # FIT MODELS -------------------------------------------------------------------------------------------------------
+  dd_hyperbo_fit_02 <- dd_hyperbolic_stan$sample(
+    data = dd_model_dat_02, iter_warmup = ITER_WARMUP, iter_sampling = ITER_SAMPLING,
+    refresh = 0, chains = 4, parallel_chains = 4, adapt_delta = 0.8, step_size = 1, max_treedepth = 10,
+    save_warmup = TRUE, output_dir = NULL)
+
+  dd_hyperbo_fit_03 <- dd_hyperbolic_stan$sample(
+    data = dd_model_dat_03, iter_warmup = ITER_WARMUP, iter_sampling = ITER_SAMPLING,
+    refresh = 0, chains = 4, parallel_chains = 4, adapt_delta = 0.8, step_size = 1, max_treedepth = 10,
+    save_warmup = TRUE, output_dir = NULL)
+
+  dd_hyperbo_fit_04 <- dd_hyperbolic_stan$sample(
+    data = dd_model_dat_04, iter_warmup = ITER_WARMUP, iter_sampling = ITER_SAMPLING,
+    refresh = 0, chains = 4, parallel_chains = 4, adapt_delta = 0.8, step_size = 1, max_treedepth = 10,
+    save_warmup = TRUE, output_dir = NULL)
+
+  dd_hyperbo_fit_05 <- dd_hyperbolic_stan$sample(
+    data = dd_model_dat_05, iter_warmup = ITER_WARMUP, iter_sampling = ITER_SAMPLING,
+    refresh = 0, chains = 4, parallel_chains = 4, adapt_delta = 0.8, step_size = 1, max_treedepth = 10,
+    save_warmup = TRUE, output_dir = NULL)
+
+  dd_hyperbo_fit_06 <- dd_hyperbolic_stan$sample(
+    data = dd_model_dat_06, iter_warmup = ITER_WARMUP, iter_sampling = ITER_SAMPLING,
+    refresh = 0, chains = 4, parallel_chains = 4, adapt_delta = 0.8, step_size = 1, max_treedepth = 10,
+    save_warmup = TRUE, output_dir = NULL)
+
+
+  # MODEL RESULTS -------------------------------------------------------------------------------------------------------------
+  process_model <- function(model_fit, task_data, model_dat, wave) {
+
+    check <- convergence_check(model_fit,
+                               params = c("k", "beta"),
+                               Rhat = TRUE, ess = TRUE,
+                               trace_plot = TRUE, rank_hist = FALSE)
+
+    trace_plot <- check$trace_plot
+    loo_result <- model_fit$loo()
+
+    parameters <- get_params(subj_id = unique(task_data$subjID),
+                             model_fit = model_fit,
+                             n_subj = length(unique(task_data$subjID)),
+                             n_params = 2, param_names = c("k", "beta"))
+
+    # Get model data for current iteration
+    dd_model_dat <- get(paste0("dd_model_dat_0", wave))
+
+    # Run posterior predictive function
+    ppredicts <- posterior_predictions(model_fit = model_fit, real_dat = dd_model_dat,
+                                       n_trials = dd_model_dat$T, n_subj = dd_model_dat$N)
+
+    # Save results
+    saveRDS(list(check$Rhat, check$ess, trace_plot),
+            here::here("output", "lcid", "dd_delaydiscount", "modelfit", "dd_hyperbolic",
+                       paste0("wave", wave),
+                       paste0("dd_hyperbo_check_0", wave, ".RDS")))
+    saveRDS(loo_result,
+            here::here("output", "lcid", "dd_delaydiscount", "modelfit", "dd_hyperbolic",
+                       paste0("wave", wave),
+                       paste0("dd_hyperbo_loo_0", wave, ".RDS")))
+    saveRDS(parameters,
+            here::here("output", "lcid", "dd_delaydiscount", "modelfit", "dd_hyperbolic",
+                       paste0("wave", wave),
+                       paste0("dd_hyperbo_parameters_0", wave, ".RDS")))
+    saveRDS(ppredicts,
+            here::here("output", "lcid", "dd_delaydiscount", "modelfit", "dd_hyperbolic",
+                       paste0("wave", wave),
+                       paste0("dd_hyperbo_ppc_0", wave, ".RDS")))
+
+    # Assign outputs to environment
+    assign(paste0("dd_hyperbo_check_0", wave), check, envir = .GlobalEnv)
+    assign(paste0("dd_hyperbo_loo_0", wave), loo_result, envir = .GlobalEnv)
+    assign(paste0("dd_hyperbo_parameters_0", wave), parameters$individual_params, envir = .GlobalEnv)
+    assign(paste0("dd_hyperbo_ppc_0", wave), ppredicts, envir = .GlobalEnv)
+
+  }
+
+  # Get Model Results across Waves
+  for (wave in 2:6) {
+    model_fit <- get(paste0("dd_hyperbo_fit_0", wave))
+    task_data <- get(paste0("task_data_0", wave))
+    model_dat <- get(paste0("dd_model_dat_0", wave))
+
+    process_model(model_fit = model_fit, task_data = task_data, model_dat = model_dat, wave = wave)
+  }
+
+  # GET MODEL FITS ------------------------------------------------------------------------------------------------------------
+  dd_hyperbo_fits <- list(dd_hyperbo_fit_02, dd_hyperbo_fit_03, dd_hyperbo_fit_04, dd_hyperbo_fit_05, dd_hyperbo_fit_06)
+
+  # Read csv files for each model
+  model_fits_tot <- lapply(dd_hyperbo_fits, function(fit) {
+    cmdstanr::read_cmdstan_csv(
+      fit$output_files(),
+      variables = c("mu_k", "mu_beta")
+    )
+  }) # outputs to environment in form of 'model_fits_tot' list
+
+  # PLOTS ---------------------------------------------------------------------------------------------------------------------
+
+  # Create and save trace plots
+  trace_plots <- purrr::imap(model_fits_tot, create_trace_plot)
+
+  # Create and save density plots
+  density_plots <- purrr::imap(model_fits_tot, create_density_plot)
+
+} else {
+
+  print("DID NOT RUN MODELS, YOU CAN LOAD RESULTS FROM FILE")
+
+}
+
+
+# LOAD MODEL FIT INDICES -------------------------------------------------------------------------------------------------------
+dd_hyperbo_check_02 <- readRDS(here::here("output", "lcid", "dd_delaydiscount", "modelfit", "dd_hyperbolic", "wave2", "dd_hyperbo_check_02.RDS"))
+dd_hyperbo_check_03 <- readRDS(here::here("output", "lcid", "dd_delaydiscount", "modelfit", "dd_hyperbolic", "wave3", "dd_hyperbo_check_03.RDS"))
+dd_hyperbo_check_04 <- readRDS(here::here("output", "lcid", "dd_delaydiscount", "modelfit", "dd_hyperbolic", "wave4", "dd_hyperbo_check_04.RDS"))
+dd_hyperbo_check_05 <- readRDS(here::here("output", "lcid", "dd_delaydiscount", "modelfit", "dd_hyperbolic", "wave5", "dd_hyperbo_check_05.RDS"))
+dd_hyperbo_check_06 <- readRDS(here::here("output", "lcid", "dd_delaydiscount", "modelfit", "dd_hyperbolic", "wave6", "dd_hyperbo_check_06.RDS"))
+
+# EXAMINE MODEL FIT -----------------------------------------------------------------------------------------------------------
+
+# Rhat maximum across waves
+max(dd_hyperbo_check_02[[1]][[6]], dd_hyperbo_check_03[[1]][[6]], dd_hyperbo_check_04[[1]][[6]],
+    dd_hyperbo_check_05[[1]][[6]], dd_hyperbo_check_06[[1]][[6]])
+
+# ESS minimum across waves
+min(dd_hyperbo_check_02[[2]][[1]], dd_hyperbo_check_03[[2]][[1]], dd_hyperbo_check_04[[2]][[1]],
+    dd_hyperbo_check_05[[2]][[1]], dd_hyperbo_check_06[[2]][[1]])
+
+
+# EXAMINE POSTERIOR PREDICTIVES ---------------------------------------------------------------------------------------------
+
+ppc02 <- readRDS(here::here("output", "lcid", "dd_delaydiscount", "modelfit", "dd_hyperbolic", "wave2", "dd_hyperbo_ppc_02.RDS"))
+ppc03 <- readRDS(here::here("output", "lcid", "dd_delaydiscount", "modelfit", "dd_hyperbolic", "wave3", "dd_hyperbo_ppc_03.RDS"))
+ppc04 <- readRDS(here::here("output", "lcid", "dd_delaydiscount", "modelfit", "dd_hyperbolic", "wave4", "dd_hyperbo_ppc_04.RDS"))
+ppc05 <- readRDS(here::here("output", "lcid", "dd_delaydiscount", "modelfit", "dd_hyperbolic", "wave5", "dd_hyperbo_ppc_05.RDS"))
+ppc06 <- readRDS(here::here("output", "lcid", "dd_delaydiscount", "modelfit", "dd_hyperbolic", "wave6", "dd_hyperbo_ppc_06.RDS"))
+
+cor(ppc02$mean_pred_choice, ppc02$mean_real_choice);
+cor(ppc03$mean_pred_choice, ppc03$mean_real_choice);
+cor(ppc04$mean_pred_choice, ppc04$mean_real_choice);
+cor(ppc05$mean_pred_choice, ppc05$mean_real_choice);
+cor(ppc06$mean_pred_choice, ppc06$mean_real_choice);
+
+ppc02 %>%
+  group_by(real_delay_later) %>%
+  summarise(correlation = cor(mean_pred_choice, mean_real_choice))
+
+ppc03 %>%
+  group_by(real_delay_later) %>%
+  summarise(correlation = cor(mean_pred_choice, mean_real_choice))
+
+ppc04 %>%
+  group_by(real_delay_later) %>%
+  summarise(correlation = cor(mean_pred_choice, mean_real_choice))
+
+ppc05 %>%
+  group_by(real_delay_later) %>%
+  summarise(correlation = cor(mean_pred_choice, mean_real_choice))
+
+ppc06 %>%
+  group_by(real_delay_later) %>%
+  summarise(correlation = cor(mean_pred_choice, mean_real_choice))
+
+## ============================================================================================================================ ##
+## DESCRIPTIVES FOR DELAY DISCOUNTING DATA
+## ============================================================================================================================ ##
+
+data_list <- list(task_data_02, task_data_03, task_data_04, task_data_05, task_data_06)
+
+# get the acceptance proportion for entire task (average of participants)
+prop_accept <- map(data_list, ~ .x %>%
+      group_by(subjID) %>%
+      summarise(
+        prop_accept = mean(choice)
+      ) %>%
+      summarise(
+        mean_accept = format(mean(prop_accept), nsmall = 5),
+        sd_accept = format(sd(prop_accept), nsmall = 5)
+      ))
+
+# get the acceptance proportion for bins within task
+prop_accept_bins <- map(data_list, ~ .x %>%
+  ungroup() %>%
+  mutate(v_diff = amount_later - amount_sooner,
+         v_diff_bin = ntile(v_diff, 4)) %>%
+  group_by(v_diff_bin) %>%
+  summarise(
+    prop_accept = format(mean(choice), nsmall = 5)
+  ))
+
+# above trend can be explained by negative association between sooner offer size and delay later
+map(data_list, ~ .x %>%
+      ungroup() %>%
+      summarise(v_corr = format(cor(x = amount_sooner, y = delay_later), nsmall = 5)))
