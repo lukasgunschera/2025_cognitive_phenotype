f7f70332bf4edf026e03ca0d3b0e83c44b107788
Lukas Gunschera <l.gunschera@outlook.com>
2025-02-26 13:25:14 +0000

    add postprocessing script

---
+++ code/lcid_postprocessing_task.R
@@ -0,0 +1,201 @@
+## ============================================================================================================================ ##
+## Script:    COMBINE PROCESSED AND MODELING DATA
+## ============================================================================================================================ ##
+## Authors:   Lukas J. Gunschera
+## Date:      Fri Apr 26 11:55:04 2024
+## ============================================================================================================================ ##
+##
+## ============================================================================================================================ ##
+
+rm(list = ls())
+set.seed(777)
+
+## ============================================================================================================================ ##
+# SCRIPT SETUP --------------------------------------------------------------------------------------------------------------
+## ============================================================================================================================ ##
+
+required_packages <- c("ggplot2","here","dplyr","magrittr","purrr","readr","stringr","viridisLite","haven","tidyr","psych","PupillometryR","validate","errorlocate")
+
+invisible(lapply(required_packages, library, character.only = TRUE))
+rm(required_packages)
+
+here::i_am("README.md")     # set parent directory to README.md file of project
+here::here()
+
+source(here("code", "lcid_delaydiscount", "functions", "fun_plots.R"))
+source(here("code", "lcid_delaydiscount", "functions", "fun_helper.R"))
+source(here("code", "lcid_delaydiscount", "functions", "fun_convergence_check.R"))
+source(here("code", "lcid_delaydiscount", "functions", "fun_load_model_results.R"))
+
+## ============================================================================================================================ ##
+# LOAD PROCESSED DATA -------------------------------------------------------------------------------------------------------
+## ============================================================================================================================ ##
+# dd_data             = contains behavioural task data (list)
+# dd_w02 ... dd_w06   = contains behavioural task data for each wave (df)
+# datq05 ... datq07   = contains questionnaire results (processed in lcid_dd_preprocessing.R) (df)
+# dat_qes             = contains all questionnaire data across waves (df)
+# dat_dem             = contains all demographics data (df)
+# dat_master          = contains merged questionnaire and demographics data (df)
+# mod_results         = contains dataframes of each measurement wave model parameters (list)
+
+## Behavioural Task Data ------------------------------------------------------------------------------------------------------ ##
+dd_data <- readRDS(here("data", "lcid", "dd_delaydiscount", "processed", "dd_task_nonzero.Rds"))
+dd_w02 <- dd_data[[1]]; dd_w03 <- dd_data[[2]]; dd_w04 <- dd_data[[3]]; dd_w05 <- dd_data[[4]]; dd_w06 <- dd_data[[5]]
+
+## Questionnaire and Demographics --------------------------------------------------------------------------------------------- ##
+dat_master <- read.csv(file = here::here("data", "lcid", "processed", "masterfile.csv"), header = TRUE)
+
+## ============================================================================================================================ ##
+# TIME-INVARIANT MASTERDAT (TVAR) -------------------------------------------------------------------------------------------
+## ============================================================================================================================ ##
+
+mod_results <- load_model_results(data_location = "local", timeinvariant = FALSE, folder = "dd_hyperbolic_nonzero")
+
+dd_hyperbo_params_02 <- mod_results$dd_hyperbo_params_02$individual_params %>%
+  tidyr::pivot_wider(names_from = parameter, values_from = c(estimate, hdi_lower, hdi_upper)) %>%
+  dplyr::mutate(subjID = subj_id) %>%
+  select(subjID, everything(), -subj_id) %>%
+  rename_with(~ paste0("w02_", .), -subjID)
+dd_hyperbo_params_03 <- mod_results$dd_hyperbo_params_03$individual_params %>%
+  tidyr::pivot_wider(names_from = parameter, values_from = c(estimate, hdi_lower, hdi_upper)) %>%
+  dplyr::mutate(subjID = subj_id) %>%
+  select(subjID, everything(), -subj_id) %>%
+  rename_with(~ paste0("w03_", .), -subjID)
+dd_hyperbo_params_04 <- mod_results$dd_hyperbo_params_04$individual_params %>%
+  tidyr::pivot_wider(names_from = parameter, values_from = c(estimate, hdi_lower, hdi_upper)) %>%
+  dplyr::mutate(subjID = subj_id) %>%
+  select(subjID, everything(), -subj_id) %>%
+  rename_with(~ paste0("w04_", .), -subjID)
+dd_hyperbo_params_05 <- mod_results$dd_hyperbo_params_05$individual_params %>%
+  tidyr::pivot_wider(names_from = parameter, values_from = c(estimate, hdi_lower, hdi_upper)) %>%
+  dplyr::mutate(subjID = subj_id) %>%
+  select(subjID, everything(), -subj_id) %>%
+  rename_with(~ paste0("w05_", .), -subjID)
+dd_hyperbo_params_06 <- mod_results$dd_hyperbo_params_06$individual_params %>%
+  tidyr::pivot_wider(names_from = parameter, values_from = c(estimate, hdi_lower, hdi_upper)) %>%
+  dplyr::mutate(subjID = subj_id) %>%
+  select(subjID, everything(), -subj_id) %>%
+  rename_with(~ paste0("w06_", .), -subjID)
+
+ddtvar <- dd_hyperbo_params_02 %>%
+  dplyr::left_join(., dd_hyperbo_params_03, by = "subjID") %>%
+  dplyr::left_join(., dd_hyperbo_params_04, by = "subjID") %>%
+  dplyr::left_join(., dd_hyperbo_params_05, by = "subjID") %>%
+  dplyr::left_join(., dd_hyperbo_params_06, by = "subjID") %>%
+  dplyr::left_join(., dat_master, by = "subjID") %>%
+  #Drop columns that are all NA
+  select(where(~!all(is.na(.))))
+
+# Remove trailing "_c" from age columns
+ddtvar %<>%
+  rename_with(~str_replace_all(., "_c", ""), contains("age"))
+
+# Add log-transformed delay discounting parameter k for each wave
+ddtvar %<>%
+  mutate(
+    w02_logk = -log(w02_estimate_k),
+    w02_logk_hdi_lower = -log(w02_hdi_lower_k),
+    w02_logk_hdi_upper = -log(w02_hdi_upper_k),
+    w03_logk = -log(w03_estimate_k),
+    w03_logk_hdi_lower = -log(w03_hdi_lower_k),
+    w03_logk_hdi_upper = -log(w03_hdi_upper_k),
+    w04_logk = -log(w04_estimate_k),
+    w04_logk_hdi_lower = -log(w04_hdi_lower_k),
+    w04_logk_hdi_upper = -log(w04_hdi_upper_k),
+    w05_logk = -log(w05_estimate_k),
+    w05_logk_hdi_lower = -log(w05_hdi_lower_k),
+    w05_logk_hdi_upper = -log(w05_hdi_upper_k),
+    w06_logk = -log(w06_estimate_k),
+    w06_logk_hdi_lower = -log(w06_hdi_lower_k),
+    w06_logk_hdi_upper = -log(w06_hdi_upper_k)
+  ) %>%
+  select(subjID, contains("logk"), contains("_k"), everything())
+
+# create long dataframe for plotting
+ddtvar_long <- ddtvar %>%
+  pivot_longer(
+    cols = contains("w0"),
+    names_to = c("wave", ".value"),
+    names_pattern = "w(\\d+)_(.*)"
+  ) %>%
+  dplyr::select(subjID, wave, everything()) %>%
+  mutate(wave = as.numeric(wave)) %>%
+  arrange(subjID, wave) %>%
+  select(subjID, wave, contains("logk"), contains("_k"), everything())
+
+## Save TVAR processed data --------------------------------------------------------------------------------------------------- ##
+write.csv(ddtvar, file = here::here("data", "lcid", "dd_delaydiscount", "processed", "tvar_masterdat_wide.csv"))
+write.csv(ddtvar_long, file = here::here("data", "lcid", "dd_delaydiscount", "processed", "tvar_masterdat_long.csv"))
+
+## ============================================================================================================================ ##
+# TIME-INVARIANT MASTERDAT (TINVAR) -----------------------------------------------------------------------------------------
+## ============================================================================================================================ ##
+
+mod_results_tinvar <- load_model_results(data_location = "local", timeinvariant = TRUE)
+
+dd_hyperbo_params_tinvar <- mod_results_tinvar$dd_hyperbo_tinvar_params$individual_params %>%
+  tidyr::pivot_wider(names_from = parameter, values_from = c(estimate, hdi_lower, hdi_upper)) %>%
+  dplyr::mutate(subjID = subj_id) %>%
+  select(subjID, everything(), -subj_id)
+
+# Remove subjIDs not included in tinvar parameters
+ddtinvar <-  dplyr::left_join(dd_hyperbo_params_tinvar, dat_master, by = "subjID") %>%
+  #Drop columns that are all NA
+  select(where(~!all(is.na(.))))
+
+# Check above remove for correctness
+ddtinvar$subjID %in% dd_hyperbo_params_tinvar$subjID %>% table()
+
+# Remove trailing "_c" from age columns
+ddtinvar %<>%
+  rename_with(~str_replace_all(., "_c", ""), contains("age"))
+
+# Add log-transformed delay discounting parameter k
+ddtinvar %<>%
+  mutate(
+    logk = -log(estimate_k),
+    logk_hdi_lower = -log(hdi_lower_k),
+    logk_hdi_upper = -log(hdi_upper_k)
+  ) %>%
+  select(subjID, contains("logk"), contains("_k"), everything())
+
+# create long dataframe for plotting
+ddtinvar_long <- ddtinvar %>%
+  pivot_longer(
+    cols = contains("w0"),
+    names_to = c("wave", ".value"),
+    names_pattern = "w(\\d+)_(.*)"
+  ) %>%
+  dplyr::select(subjID, wave, everything()) %>%
+  mutate(wave = as.numeric(wave)) %>%
+  arrange(subjID, wave)
+
+## Save TINVAR processed data ------------------------------------------------------------------------------------------------- ##
+write.csv(ddtinvar, file = here::here("data", "lcid", "dd_delaydiscount", "processed", "tinvar_masterdat_wide.csv"))
+write.csv(ddtinvar_long, file = here::here("data", "lcid", "dd_delaydiscount", "processed", "tinvar_masterdat_long.csv"))
+
+## ============================================================================================================================ ##
+# ERROR CHECKS --------------------------------------------------------------------------------------------------------------
+## ============================================================================================================================ ##
+
+## Check Age Ranges =========================================================================================================== ##
+summary(validate::confront(ddtvar, validate::validator(    # TIME-VARIANT
+  r1 = w01_age >= 6.75,
+  r2 = w02_age >= 7.75,
+  r3 = w03_age >= 8.75,
+  r4 = w04_age >= 9.75,
+  r5 = w05_age >= 10.75,
+  r6 = w06_age >= 11.75,
+  r7 = w07_age >= 12.75
+)))
+
+summary(confront(ddtinvar, validate::validator(  # TIME-INVARIANT
+  r1 = w01_age >= 6.75,
+  r2 = w02_age >= 7.75,
+  r3 = w03_age >= 8.75,
+  r4 = w04_age >= 9.75,
+  r5 = w05_age >= 10.75,
+  r6 = w06_age >= 11.75,
+  r7 = w07_age >= 12.75
+)))
+
