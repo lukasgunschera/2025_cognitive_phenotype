71c62604a523994d74777526a334211b80a687b0
lukasgunschera <l.gunschera@outlook.com>
2025-03-27 14:05:17 +0000

    change default parameters

--- code/lcid_posterior_predictive.R
+++ code/lcid_posterior_predictive.R
@@ -16,9 +16,9 @@ set.seed(777)
 
 # set global parameters determining the model fitting process
 
-SIMULATE_DATA <- TRUE  # should data be simulated or a previous simulation loaded
+SIMULATE_DATA <- FALSE  # should data be simulated or a previous simulation loaded
 FIT_CLUSTER <- FALSE   # should the fitting be run on a cluster or locally
-FIT_MODEL <- TRUE      # should the fitting be run or loaded from file
+FIT_MODEL <- FALSE      # should the fitting be run or loaded from file
 N_SAMPLE <- 10000       # number of participants to simulate
 
 # load required packages
@@ -69,65 +69,111 @@ ppc_table <- bind_rows(ppc02_corr, ppc03_corr, ppc04_corr, ppc05_corr, ppc06_cor
   knitr::kable(., caption = "Correlations of predicted and real choices", align = "l", digits = 3)
 
 ## Visualise Posterior Predictive Checks -------------------------------------------------------------------------------------
-ppc02 %>%
-  ggplot(aes(x = mean_real_choice, y = mean_pred_choice)) +
-  geom_point(shape = 20, alpha = .8, aes(colour = hdi_upper_pred_choice - hdi_lower_pred_choice),
-             position = position_jitter(width = .05)) +
+ppcplot2 <- ppc02 %>%
+  dplyr::mutate(real_delay_later = factor(real_delay_later)) %>%
+  ggplot(aes(x = mean_real_choice, y = mean_pred_choice, colour = real_delay_later)) +
+  geom_point(shape = 16, alpha = .75, position = position_jitter(width = .05), size = 3) +
   geom_smooth(method = "lm", color = "#2E2E2E", fullrange = TRUE) +
   plot_theme +
   aspect_ratio_square +
-  scale_colour_viridis(name = "HDI Width", direction = 1) +
-  scale_x_continuous(breaks = seq(0,1,.2), expand = c(0.05, 0.025)) +
-  scale_y_continuous(breaks = seq(0,1,.2), expand = c(0.05, 0.05)) +
-  labs(x = "Observed Choice", y = "Predicted Choice") +
+  scale_colour_viridis_d(direction = 1) +
+  scale_x_continuous(breaks = seq(0,1,.25), expand = c(0.05, 0.025)) +
+  scale_y_continuous(breaks = seq(0,1,.25), expand = c(0.05, 0.05)) +
+  labs(x = "Observed", y = "Predicted") +
   annotate(x = -Inf, xend = -Inf, y = 0, yend = 1, colour = "#2E2E2E", lwd = 0.75, geom = "segment") +
-  annotate(x = 0, xend = 1, y = -Inf, yend = -Inf, colour = "#2E2E2E", lwd = 0.75, geom = "segment") +
-  stat_poly_eq(use_label(c("R2")), label.y = 0.925, size = 8, colour = "#2E2E2E", parse = TRUE)
+  annotate(x = 0, xend = 1, y = -Inf, yend = -Inf, colour = "#2E2E2E", lwd = 0.75, geom = "segment")
 
-ppc_plots <- lapply(list(ppc02, ppc03, ppc04, ppc05, ppc06), FUN = ppc_plot)
-ppcs <- list(ppc02, ppc03, ppc04, ppc05, ppc06)
-ppc_plots <- list()
-for(x in 1:5){
-  ppc_plots[[x]] <- ppc_plot(ppcs[[x]]) +
-    ggtitle(paste("Wave", x+1, sep = " ")) +
-    theme(plot.title = element_text(vjust = -1, size = 10))
-}
+ppcplot3 <- ppc03 %>%
+  dplyr::mutate(real_delay_later = factor(real_delay_later)) %>%
+  ggplot(aes(x = mean_real_choice, y = mean_pred_choice, colour = real_delay_later)) +
+  geom_point(shape = 16, alpha = .75, position = position_jitter(width = .05), size = 3) +
+  geom_smooth(method = "lm", color = "#2E2E2E", fullrange = TRUE) +
+  plot_theme +
+  aspect_ratio_square +
+  scale_colour_viridis_d(direction = 1) +
+  scale_x_continuous(breaks = seq(0,1,.25), expand = c(0.05, 0.025)) +
+  scale_y_continuous(breaks = seq(0,1,.25), expand = c(0.05, 0.05)) +
+  labs(x = "Observed", y = "Predicted") +
+  annotate(x = -Inf, xend = -Inf, y = 0, yend = 1, colour = "#2E2E2E", lwd = 0.75, geom = "segment") +
+  annotate(x = 0, xend = 1, y = -Inf, yend = -Inf, colour = "#2E2E2E", lwd = 0.75, geom = "segment")
 
-ppc_plot(ppc02)
+ppcplot4 <- ppc04 %>%
+  dplyr::mutate(real_delay_later = factor(real_delay_later)) %>%
+  ggplot(aes(x = mean_real_choice, y = mean_pred_choice, colour = real_delay_later)) +
+  geom_point(shape = 16, alpha = .75, position = position_jitter(width = .05), size = 3) +
+  geom_smooth(method = "lm", color = "#2E2E2E", fullrange = TRUE) +
+  plot_theme +
+  aspect_ratio_square +
+  scale_colour_viridis_d(direction = 1) +
+  scale_x_continuous(breaks = seq(0,1,.25), expand = c(0.05, 0.025)) +
+  scale_y_continuous(breaks = seq(0,1,.25), expand = c(0.05, 0.05)) +
+  labs(x = "Observed", y = "Predicted") +
+  annotate(x = -Inf, xend = -Inf, y = 0, yend = 1, colour = "#2E2E2E", lwd = 0.75, geom = "segment") +
+  annotate(x = 0, xend = 1, y = -Inf, yend = -Inf, colour = "#2E2E2E", lwd = 0.75, geom = "segment")
 
+ppcplot5 <- ppc05 %>%
+  dplyr::mutate(real_delay_later = factor(real_delay_later)) %>%
+  ggplot(aes(x = mean_real_choice, y = mean_pred_choice, colour = real_delay_later)) +
+  geom_point(shape = 16, alpha = .75, position = position_jitter(width = .05), size = 3) +
+  geom_smooth(method = "lm", color = "#2E2E2E", fullrange = TRUE) +
+  plot_theme +
+  aspect_ratio_square +
+  scale_colour_viridis_d(direction = 1) +
+  scale_x_continuous(breaks = seq(0,1,.25), expand = c(0.05, 0.025)) +
+  scale_y_continuous(breaks = seq(0,1,.25), expand = c(0.05, 0.05)) +
+  labs(x = "Observed", y = "Predicted") +
+  annotate(x = -Inf, xend = -Inf, y = 0, yend = 1, colour = "#2E2E2E", lwd = 0.75, geom = "segment") +
+  annotate(x = 0, xend = 1, y = -Inf, yend = -Inf, colour = "#2E2E2E", lwd = 0.75, geom = "segment")
 
-blank <- ggplot() + theme_void()
-ppc_plots <- list(ppc_plots[[1]] + rremove("xlab"),
-                  ppc_plots[[2]] + rremove("xlab") + rremove("ylab"),
-                  ppc_plots[[3]] + rremove("xlab") + rremove("ylab"),
-                  ppc_plots[[4]],
-                  ppc_plots[[5]] + rremove("ylab"))
+ppcplot6 <- ppc06 %>%
+  dplyr::mutate(real_delay_later = factor(real_delay_later)) %>%
+  ggplot(aes(x = mean_real_choice, y = mean_pred_choice, colour = real_delay_later)) +
+  geom_point(shape = 16, alpha = .75, position = position_jitter(width = .05), size = 3) +
+  geom_smooth(method = "lm", color = "#2E2E2E", fullrange = TRUE) +
+  plot_theme +
+  aspect_ratio_square +
+  scale_colour_viridis_d(direction = 1) +
+  scale_x_continuous(breaks = seq(0,1,.25), expand = c(0.05, 0.025)) +
+  scale_y_continuous(breaks = seq(0,1,.25), expand = c(0.05, 0.05)) +
+  labs(x = "Observed", y = "Predicted") +
+  annotate(x = -Inf, xend = -Inf, y = 0, yend = 1, colour = "#2E2E2E", lwd = 0.75, geom = "segment") +
+  annotate(x = 0, xend = 1, y = -Inf, yend = -Inf, colour = "#2E2E2E", lwd = 0.75, geom = "segment")
 
+ppcplot6 <- ppcplot6 +
+  theme(legend.position = "right") +
+  labs(colour = "Delay level") +
+  ggtitle("Wave 6") +
+  theme(plot.title = element_text(vjust = -5, size = 12))
 
-ppc_plots_combined <- ggpubr::ggarrange(
-  ggpubr::ggarrange(ppc_plots[[1]], ppc_plots[[2]], ppc_plots[[3]], blank, nrow = 1,
-            widths = c(1, 1, 1, 0), legend = "none"),
-  ggpubr::ggarrange(ppc_plots[[4]], ppc_plots[[5]], blank, blank, nrow = 1,
-            widths = c(1, 1, 0.5,0.5), legend = "none"),
-  ggpubr::as_ggplot(ggpubr::get_legend(ppc_plots[[1]] , position = "bottom")),
-  nrow = 3, heights = c(1, 1, 0.25),
-  legend = "none"
+ppcplot5 <- ppcplot5 +
+  ggtitle("Wave 5") +
+  theme(plot.title = element_text(vjust = -5, size = 12))
+
+ppcplot4 <- ppcplot4 +
+  ggtitle("Wave 4") +
+  theme(plot.title = element_text(vjust = -5, size = 12))
+
+ppcplot3 <- ppcplot3 +
+  ggtitle("Wave 3") +
+  theme(plot.title = element_text(vjust = -5, size = 12))
+
+ppcplot2 <- ppcplot2 +
+  ggtitle("Wave 2") +
+  theme(plot.title = element_text(vjust = -5, size = 12))
+
+# create layout of mergerd plot
+layout <- c(
+  area(1, 1),
+  area(1, 2),
+  area(1, 3),
+  area(2, 1),
+  area(2, 2)
 )
 
-ggsave(ppc_plots_combined,
-       path = here::here("output", "lcid", "dd_delaydiscount", "images", "08_posterior_predictive"),
+# combine the plots using the defined layout
+combined_plot <- ppcplot2 + ppcplot3 + ppcplot4 + ppcplot5 + ppcplot6 + plot_spacer() +
+  plot_layout(design = layout, guides = "collect")
+
+ggsave(combined_plot,
+       path = here::here("output", "images", "modelfit"),
        filename = "ppc_corrplots.png", dpi = 1200, device = "png")
-
-ggppc1 <- ppc_plots[[1]]; ggppc2 <- ppc_plots[[2]]; ggppc3 <- ppc_plots[[3]]; ggppc4 <- ppc_plots[[4]]; ggppc5 <- ppc_plots[[5]]
-
-ggsave(ggppc1, path = here::here("output", "lcid", "dd_delaydiscount", "images", "08_posterior_predictive"),
-       filename = "ppc_02.png", dpi = 1200, device = "png")
-ggsave(ggppc2, path = here::here("output", "lcid", "dd_delaydiscount", "images", "08_posterior_predictive"),
-       filename = "ppc_03.png", dpi = 1200, device = "png")
-ggsave(ggppc3, path = here::here("output", "lcid", "dd_delaydiscount", "images", "08_posterior_predictive"),
-       filename = "ppc_04.png", dpi = 1200, device = "png")
-ggsave(ggppc4, path = here::here("output", "lcid", "dd_delaydiscount", "images", "08_posterior_predictive"),
-       filename = "ppc_05.png", dpi = 1200, device = "png")
-ggsave(ggppc5, path = here::here("output", "lcid", "dd_delaydiscount", "images", "08_posterior_predictive"),
-       filename = "ppc_06.png", dpi = 1200, device = "png")
-
